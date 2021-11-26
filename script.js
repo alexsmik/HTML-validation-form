83c43b4fe15a2b73d39efb3de811aea8dcff3d1b
alexsmik <alexsmik@yandex.ru>
2021-11-26 19:39:16 +0300

    mistakes in style file

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const passwordRepeat = document.getElementById('passwordRepeat');
+const password2 = document.getElementById('password2');
 // listen
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    checkRequired([username, email, password, passwordRepeat]);
+    checkRequired([username, email, password, password2]);
     checkLength(username, 2, 6);
     checkLength(password, 4, 8);
     checkEmail(email);
@@ -16,15 +16,17 @@ form.addEventListener('submit', function(e) {
 // show error message
 function showError(input, message) {
     const formControl = input.parentElement;
-    formControl.className = 'form error';
+    formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
+
 // show valid
 function showSuccess(input) {
     const formControl = input.parentElement;
-    formControl.className = 'form success';
+    formControl.className = 'form-control success';
 }
+
 // checking email by regexp
 function checkEmail(input) {
     const regx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -34,6 +36,7 @@ function checkEmail(input) {
         showError( input, 'Почта введена неверно');
     }
 }
+
 // checking required fields as array
 function checkRequired(checkInputsInArr) {
     checkInputsInArr.forEach((input) => {
@@ -44,6 +47,7 @@ function checkRequired(checkInputsInArr) {
         }
     });
 }
+
 // input length for some fields
 function checkLength(input, min, max) {
     function wordOv() {
@@ -66,19 +70,21 @@ function checkLength(input, min, max) {
         showSuccess(input);
     }
 }
+
 // password matching
-function checkPasswordsMatch(password, passwordRepeat) {
-    if (password.value !== passwordRepeat.value) {
-        showError(passwordRepeat, 'Пароли не совпадают');
+function checkPasswordsMatch(password, password2) {
+    if (password.value !== password2.value) {
+        showError(password2, 'Пароли не совпадают');
     }
 }
+
 // fields for translate
 function fieldName(input) {
     if (input === email) {
         return 'Почта';
     } else if (input === password) {
         return 'Пароль';
-    } else if (input === passwordRepeat) {
+    } else if (input === password2) {
         return 'Пароль повторно';
     } else if (input === username) {
         return 'Пользователь';
